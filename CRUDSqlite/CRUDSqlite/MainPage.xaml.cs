2f0676026fa81735b67c46a27357b6316a3ba813
BRYVERA <bryan.rivera.git@gmail.com>
2022-02-09 23:17:59 -0600

    First commit

---
+++ CRUDSqlite/CRUDSqlite/MainPage.xaml.cs
@@ -0,0 +1,173 @@
+﻿using CRUDSqlite.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace CRUDSqlite
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            llenarDatos();
+        }
+
+        private async void btnRegistrar_Clicked(object sender, EventArgs e)
+        {
+            if (validarDatos())
+            {
+                Alumno alum = new Alumno
+                {
+                    Nombre = txtNombre.Text,
+                    ApellidoPaterno = txtApellidoPaterno.Text,
+                    ApellidoMaterno = txtApellidoMaterno.Text,
+                    Edad = int.Parse(txtEdad.Text),
+                    Email = txtEmail.Text,
+                };
+                await App.SQLiteDB.SaveAlumnoAsync(alum);
+
+                
+                await DisplayAlert("Registro", "Almacenado Correctamente", "OK");
+                LimpiarControles();
+                llenarDatos();
+               
+
+            }
+            else
+            {
+                await DisplayAlert("Advertencia", "Ingresar todos los datos", "OK");
+            }
+        }
+
+        public async void llenarDatos()
+        {
+            var alumnoList = await App.SQLiteDB.GetAlumnosAsync();
+            if (alumnoList != null)
+            {
+                lstAlumnos.ItemsSource = alumnoList;
+            }
+        }
+
+        public bool validarDatos()
+        {
+            bool respuesta;
+
+            if (string.IsNullOrEmpty(txtNombre.Text))
+            {
+                respuesta = false;
+            }
+            else if (string.IsNullOrEmpty(txtApellidoPaterno.Text))
+            {
+                respuesta = false;
+            }
+            else if (string.IsNullOrEmpty(txtApellidoMaterno.Text))
+            {
+                respuesta = false;
+            }
+            else if (string.IsNullOrEmpty(txtEdad.Text))
+            {
+                respuesta = false;
+            }
+            else if (string.IsNullOrEmpty(txtEmail.Text))
+            {
+                respuesta = false;
+            }
+            else
+            {
+                respuesta = true;
+            }
+            return respuesta;
+        }
+
+        private async void lstAlumnos_ItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+
+            var obj = (Alumno)e.SelectedItem;
+            btnRegistrar.IsVisible = false;
+            txtidAlumno.IsVisible = true;
+            btnActualizar.IsVisible = true;
+            btnEliminar.IsVisible = true;
+
+            if (!string.IsNullOrEmpty(obj.idAlumno.ToString()))
+            {
+                var alumno = await App.SQLiteDB.GetAlumnoByIdAsync(obj.idAlumno);
+
+                if (alumno != null)
+                {
+                    txtidAlumno.Text = alumno.idAlumno.ToString();
+                    txtNombre.Text = alumno.Nombre;
+                    txtApellidoPaterno.Text = alumno.ApellidoPaterno;
+                    txtApellidoMaterno.Text = alumno.ApellidoMaterno;
+                    txtEdad.Text = alumno.Edad.ToString();
+                    txtEmail.Text = alumno.Email;
+                }
+            }
+
+        }
+
+        private async void btnActualizar_Clicked(object sender, EventArgs e)
+        {
+
+            if (!string.IsNullOrEmpty(txtidAlumno.Text))
+            {
+                Alumno alumno = new Alumno()
+                {
+                    idAlumno = Convert.ToInt32(txtidAlumno.Text), 
+                    Nombre = txtNombre.Text,
+                    ApellidoPaterno = txtApellidoPaterno.Text,
+                    ApellidoMaterno = txtApellidoMaterno.Text,
+                    Edad = Convert.ToInt32(txtEdad.Text),
+                    Email = txtEmail.Text,
+                };
+                await App.SQLiteDB.SaveAlumnoAsync(alumno);
+                await DisplayAlert("Registro", "Se actualizo correctamente", "OK");
+
+
+                LimpiarControles();
+                txtidAlumno.IsVisible = false;
+                btnActualizar.IsVisible = false;
+                btnRegistrar.IsVisible = true;
+
+                llenarDatos();
+            }
+
+        }
+
+        private async void btnEliminar_Clicked(object sender, EventArgs e)
+        {
+
+            var alumno = await App.SQLiteDB.GetAlumnoByIdAsync(Convert.ToInt32(txtidAlumno.Text));
+
+            if (alumno != null)
+            {
+                await App.SQLiteDB.DeleteAlumnoAsync(alumno);
+                await DisplayAlert("Alumno", "Se elimino", "Ok");
+                LimpiarControles();
+                llenarDatos();
+                txtidAlumno.IsVisible = false;
+                btnActualizar.IsVisible = false;
+                btnEliminar.IsVisible = false;
+                btnRegistrar.IsVisible = true;
+
+            }
+
+        }
+
+        public void LimpiarControles()
+        {
+            txtidAlumno.Text = "";
+            txtNombre.Text = "";
+            txtApellidoPaterno.Text = "";
+            txtApellidoMaterno.Text = "";
+            txtEdad.Text = "";
+            txtEmail.Text = "";
+        }
+
+    }
+
+}
---
+++ CRUDSqlite/CRUDSqlite/Models/Alumno.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SQLite;
+
+namespace CRUDSqlite.Models
+{
+    public class Alumno
+    {
+        [PrimaryKey,AutoIncrement]
+        public int idAlumno { get; set; }
+        [MaxLength(30)]
+        public string Nombre { get; set; }
+        [MaxLength(30)]
+        public string ApellidoPaterno { get; set; }
+        [MaxLength(30)]
+        public string ApellidoMaterno { get; set; }
+        
+        public int Edad { get; set; }
+        [MaxLength(40)]
+        public string Email { get; set; }
+    }
+}
